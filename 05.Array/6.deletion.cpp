1a79026a2014fef40691a3009a82eda98a037316
Shivam Yadav <your-shivamyadav201204@gmail.com>
2025-03-10 15:52:01 +0530

    updated

---
+++ 05.Array/6.deletion.cpp
@@ -0,0 +1,23 @@
+//i->12
+//i=i+1
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+void deletedata(arr[],int 12){
+    for(int i=0,i<n,i++){
+        if (arr[i]=12)
+        {
+            for(int j=i,j<n,j++)
+            arr[i]=arr[]
+        }
+        [i]=12;
+        cout<<
+    }
+}
+
+int main(){
+    int arr[4]={10,12,343,53};
+    deletedata(arr[],4);
+    return 0;
+}
